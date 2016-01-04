43c731fe0b042d2bd5a84eec659d2f85ee7ee411
“Evan <evan@datadoghq.com>
2016-01-04 12:52:56 -0500

    devstack scripts
    scripts for deploying DevStack to Digital Ocean

---
+++ openstack/devstack/deploy_droplet.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import os  # For running system commands
+import sys  # For command line arguments
+
+# Find your SSH key ID using 'tugboat keys'
+
+
+def main(args):
+    if args == '':
+        args = raw_input("You must name your droplet: ")
+        main(args)
+    tug_com = "tugboat create " + args + " -s 4gb -i 14782728"
+    # -s 4gb is the instance RAM size, -i 14782728 is the Ubuntu 14.04 x64 image
+    # You can see a list of images with "tugboat images"
+    os.system(tug_com)
+    # Wait for droplet to become active
+    os.system("tugboat wait " + args + " -s")
+    # Give it a couple of seconds after it reports activity
+    os.system("sleep 8")
+    t = os.popen("tugboat droplets | grep " + args)
+    t = t.read()
+    IP = t[t.find('ip') + 4 : t.find(',')]
+    print "IP: " + IP
+
+    command_to_run = "ssh root@" + IP + " 'bash -s' < stack_setup.sh"
+    print "Run the following (in order): \n" + command_to_run
+    print "ssh root@" + IP
+    print "cd /usr/local/src/devstack && su stack"
+    print "./stack.sh"
+
+if __name__ == "__main__":
+    try:
+        main(sys.argv[1])
+    except IndexError:
+        name = raw_input("Name your droplet: ")
+        main(name)
---
+++ openstack/devstack/stack_setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+if [ `whoami`  != 'root' ]; then echo "Run as root"; exit; fi
+apt-get update && apt-get install git -y
+cd /usr/local/src
+git clone -b stable/kilo https://github.com/openstack-dev/devstack.git
+cd devstack
+./tools/create-stack-user.sh
+echo "[[local|localrc]]
+disable_service n-net
+enable_service q-svc
+enable_service q-agt
+enable_service q-dhcp
+enable_service q-l3
+enable_service q-meta
+
+# Optional, to enable tempest configuration as part of devstack
+enable_service tempest
+
+# Enable the ceilometer services
+enable_service ceilometer-acompute,ceilometer-acentral,ceilometer-collector,ceilometer-api
+
+#Configure events for both Stacktach and custom listener
+notification_driver=nova.openstack.common.notifier.rpc_notifier
+notification_topics=notifications,monitor
+notify_on_state_change=vm_and_task_state
+notify_on_any_change=True
+instance_usage_audit=True
+instance_usage_audit_period=hour
+
+# Password configuration below
+ADMIN_PASSWORD=devstack
+DATABASE_PASSWORD=devstack
+RABBIT_PASSWORD=devstack
+SERVICE_PASSWORD=devstack
+SERVICE_TOKEN=a682f596-76f3-11e3-b3b2-e716f9080d50
+
+" >> local.conf
+sed -i 's,git://git.openstack.org,https://github.com,g' stackrc
+sed -i 's/install_infra/pip_install testrepository\ninstall_infra/g' stack.sh
+chown -R stack:stack .
