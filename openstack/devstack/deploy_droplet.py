0579dae3a871c26cd6a825818eac9eb66362588e
vagelim <evan@datadoghq.com>
2016-01-06 13:13:05 -0500

    updates to deployment script
    moved ram size and image to environment variables to enable user changes

--- openstack/devstack/deploy_droplet.py
+++ openstack/devstack/deploy_droplet.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python
-import os  # For running system commands
+import os  # For running system commands & env vars
 import sys  # For command line arguments
 
 # Find your SSH key ID using 'tugboat keys'
+
+# Try to get values from environment variables
+try:
+    RAM = os.environ['DO_RAM_SIZE']
+    IMAGE = os.environ['DO_IMAGE']
+
+except KeyError:
+    RAM = "4gb"  # Minimum required RAM to host ~2 instances
+    # You can see a list of sizes with "tugboat sizes"
+    IMAGE = "14782728"  # Ubuntu 14.04 x64 image
+    # You can see a list of images with "tugboat images"
 
 
 def main(args):
     if args == '':
         args = raw_input("You must name your droplet: ")
         main(args)
-    tug_com = "tugboat create " + args + " -s 4gb -i 14782728"
-    # -s 4gb is the instance RAM size, -i 14782728 is the Ubuntu 14.04 x64 image
-    # You can see a list of images with "tugboat images"
+    tug_com = "tugboat create " + args + " -s " + RAM + " -i " + IMAGE
+
     os.system(tug_com)
     # Wait for droplet to become active
     os.system("tugboat wait " + args + " -s")
